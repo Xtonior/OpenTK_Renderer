51f35bf791f33dbab4b715a2e54d49234f905ba8
Xtonior <xtonior7901@gmail.com>
2024-12-03 09:23:38 +0500

    Sphere intersection

--- Assets/Shaders/shader.vert
+++ Assets/Shaders/shader.vert
@@ -1,9 +1,12 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPosition;  // the position variable has attribute position 0
+layout(location = 0) in vec3 aPosition;  
+layout(location = 1) in vec2 aTexCoord;
+
+out vec2 texCoord;
 
 void main(void)
 {
-	// see how we directly give a vec3 to vec4's constructor
-    gl_Position = vec4(aPosition, 1.0); 
+    texCoord = vec2(aPosition);
+    gl_Position = vec4(aPosition, 1.0);
 }
--- Game.cs
+++ Game.cs
@@ -10,13 +10,19 @@ namespace Engine.Game
         private Shader? mainShader;
 
         float[] quadVertices = {
-        -1.0f, -1.0f, 0.0f,  // Bottom-left
-        1.0f, -1.0f, 0.0f,  // Bottom-right
-        -1.0f,  1.0f, 0.0f,  // Top-left
-        1.0f,  1.0f, 0.0f   // Top-right
+    // Positions   // Texture Coords
+     1.0f,  1.0f, 1.0f, 1.0f, // Bottom-left
+     1.0f, -1.0f, 1.0f, 1.0f, // Bottom-right
+    -1.0f, -1.0f, 1.0f, 1.0f, // Top-left
+    -1.0f,  1.0f, 1.0f, 1.0f, // Top-left
+};
+
+        int[] indices = {
+            0, 1, 3,
+            1, 2, 3
         };
 
-        int vbo, vao;
+        int vbo, vao, ebo;
 
         public override void OnLoad()
         {
@@ -24,17 +30,30 @@ namespace Engine.Game
 
             vao = GL.GenVertexArray();
             vbo = GL.GenBuffer();
+            ebo = GL.GenBuffer();
 
             GL.BindVertexArray(vao);
             GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
             GL.BufferData(BufferTarget.ArrayBuffer, quadVertices.Length * sizeof(float), quadVertices, BufferUsageHint.StaticDraw);
 
-            // Set vertex attribute pointers
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(int), indices, BufferUsageHint.StaticDraw);
 
             mainShader = new Shader("Assets/Shaders/shader.vert", "Assets/Shaders/shader.frag");
             mainShader.Use();
+
+            // Position attribute (2 floats)
+            var vertexLocation = mainShader.GetAttribLocation("aPosition");
+            GL.VertexAttribPointer(vertexLocation, 2, VertexAttribPointerType.Float, false, 4 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(vertexLocation);
+
+            // Texture coordinate attribute (2 floats)
+            var texCoordLocation = mainShader.GetAttribLocation("aTexCoord");
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 4 * sizeof(float), 2 * sizeof(float));
+            GL.EnableVertexAttribArray(texCoordLocation);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
         }
 
         public override void OnRender()
@@ -44,13 +63,12 @@ namespace Engine.Game
             mainShader.Use();
 
             int u_resolution = GL.GetUniformLocation(mainShader.Handle, "u_resolution");
-            GL.Uniform2(u_resolution, 800, 600);
-
-            int u_pos = GL.GetUniformLocation(mainShader.Handle, "u_pos");
-            GL.Uniform4(u_pos, 0.0f, 0.0f, 0.0f, 0.0f); // Sphere center at (0,0) and radius 1
+            GL.Uniform2(u_resolution, 800f, 600f);
 
             GL.BindVertexArray(vao);
-            GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
+            // GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 6); // Six vertices for two triangles
+            // GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.DrawElements(BeginMode.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
         }
 
         public override void OnUpdate() { }
