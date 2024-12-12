1bc13d77b7431a5681392a9b6c3747d08e186d5d
Xtonior <xtonior7901@gmail.com>
2024-12-12 12:12:20 +0500

    reinitialized repository

---
+++ Core/Window.cs
@@ -0,0 +1,86 @@
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using System;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace Engine.Core
+{
+    public class Window : GameWindow
+    {
+        public Action LoadAction;
+        public Action<float> UpdateFrameAction;
+        public Action RenderFrameAction;
+
+        public bool IsActiveWindow { get; set; } = true;
+
+        public float Time { get; private set; }
+
+        private Vector2 lastMousePos = Vector2.Zero;
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            VSync = VSyncMode.Off;
+            LoadAction?.Invoke();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            if (KeyboardState.IsKeyPressed(Keys.Escape))
+            {
+                ChangeActive(false);
+            }
+
+            if(MouseState.IsButtonPressed(MouseButton.Left))
+            {
+                ChangeActive(true);
+            }
+            
+            base.OnUpdateFrame(e);
+
+            UpdateFrameAction?.Invoke((float)e.Time);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            RenderFrameAction?.Invoke();
+
+            Time = (float)e.Time;
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        private void ChangeActive(bool active)
+        {
+            switch (active)
+            {
+                case true:
+                    lastMousePos = MousePosition;
+                    IsActiveWindow = true;
+                    CursorState = CursorState.Grabbed;
+                    break;
+                case false:
+                    MousePosition = lastMousePos;
+                    IsActiveWindow = false;
+                    CursorState = CursorState.Normal;
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Path Tracing 3D Renderer
+
+## About
+Hello!, I'm Xtonior and the only developer of this renderer. I'm doing it for fun, but if you want to use my repo or mention it feel free
+
+## What is done
+- Basic path tracing (ray reflection, refraction)
+- Sphere, Cube and plane intersections
+- Light sources
+- Skybox
+
+## Todo:
+- 3D polygonal mesh support
+- TAA
+- Post-Processing
+- World Editor
+- Material Editor
+- More complex materials
+- Textures (albedo, metallic, roughness and normal maps)
+
+## Contacts
+Discord: .xtonior
